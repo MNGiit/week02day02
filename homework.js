04f05dbcec5722ee95ffc5238bb1809792bb5cda
MNGiit <36828920+MNGiit@users.noreply.github.com>
2022-08-17 17:06:19 -0400

    add talk() method and properties

--- homework.js
+++ homework.js
@@ -1,11 +1,25 @@
 class Cat {
-    constructor() {
-
+    constructor(name, owner, meow = "Meow") {
+        this.name = name;
+        this.owner = owner;
+        this.meow = meow;
     }
+
+    talk() {
+        console.log(this.meow);
+    }
+
+    // method 2
+    // method 3
+
+
 }
 
-let cat1 = new Cat();
-let cat2 = new Cat();
+let cat1 = new Cat("Garfield", "Jon", "I never met a lasagna I didn't like.");
+let cat2 = new Cat("Stray Cat", "David Bowie", "Hsssssss");
 
 console.log(cat1)
 console.log(cat2)
+
+cat1.talk();
+cat2.talk();
