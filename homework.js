f7293e6c20ff7653d4f1421e5efd4244fec58da9
MNGiit <36828920+MNGiit@users.noreply.github.com>
2022-08-17 17:10:21 -0400

    add whatIsName() method

--- homework.js
+++ homework.js
@@ -5,11 +5,14 @@ class Cat {
         this.meow = meow;
     }
 
+    whatIsName() {
+        console.log("Meow. My name is", this.name)
+    }
+
     talk() {
         console.log(this.meow);
     }
 
-    // method 2
     whoIsOwner() {
         console.log("Meow. My owner is", this.owner)
     }
@@ -24,8 +27,11 @@ let cat2 = new Cat("Stray Cat", "David Bowie", "Hsssssss");
 console.log(cat1)
 console.log(cat2)
 
-cat1.talk();
-cat2.talk();
+cat1.whatIsName();
+cat2.whatIsName();
 
 cat1.whoIsOwner();
 cat2.whoIsOwner();
+
+cat1.talk();
+cat2.talk();
